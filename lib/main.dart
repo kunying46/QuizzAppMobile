25eebab77d3e3678179e603bec10e0e02dba1d2a
kunying46 <142193233+kunying46@users.noreply.github.com>
2025-02-14 11:06:54 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:myapp/%20start_screen.dart';
-
-void main() {
-  runApp(MaterialApp(
-      home: Scaffold(
-    body: Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [Colors.purple, Colors.deepPurple],
+ 
+class AnswerButton extends StatelessWidget {
+  final String answerText;
+  final void Function() onPressed;
+ 
+  const AnswerButton(this.answerText, {required this.onPressed, super.key});
+ 
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0), // เพิ่มระยะห่างแนวตั้ง
+      child: SizedBox(
+        width: double.infinity,
+        child: ElevatedButton(
+          onPressed: onPressed,
+          style: ElevatedButton.styleFrom(
+            backgroundColor: const Color.fromARGB(255, 58, 0, 97),
+            foregroundColor: Colors.white,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+          ),
+          child: Text(
+            answerText,
+            style: const TextStyle(fontSize: 16),
+            textAlign: TextAlign.center,
+          ),
         ),
       ),
-      child:  const StartScreen()
-    ),
-  )));
+    );
+  }
 }
-
